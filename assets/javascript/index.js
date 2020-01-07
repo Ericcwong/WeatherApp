c7e9dd1019dc5600fdbabb45a90bd283c17dfa71
Eric Wong <ericwong12@live.com>
2020-01-07 15:29:00 -0700

    Added icons to that is pulled from openweather API
    FAR FROM DRY but it works hahah

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -18,7 +18,7 @@ $("#getWeather").on("click",function(event){
         $(".currentDay").html("");
         $(".currentDay").append(`
         <h3 id="cityName">${response.name}</h3>
-        <p id="temperature">Temperature: ${newFahrenheit} F</p>
+        <h5 id="temperature">Temperature: ${newFahrenheit} &#8457; </p>
         <p id="humidity">Humidity: ${response.main.humidity}</p>
         <p id="windSpeed">Wind Speed: ${response.wind.speed}</p>
         <p id="uVIndex"></p>
@@ -32,18 +32,57 @@ $("#getWeather").on("click",function(event){
     }).then(function(week){
         console.log(fiveDayURL);
         console.log(week);
-        var k = response.main.temp
-        var fahrenheit = (k -273.15) * 1.80 + 32;
-        var newFahrenheit = fahrenheit.toFixed(1);
-        $("#fiveDay").html("");
-        $("#fiveDay").append(`
+        console.log(week.list[4].main.temp)
+        var dayOneIconPull = (week.list[4].weather[0].icon);
+        var dayOneIcon = "http://openweathermap.org/img/wn/"+dayOneIconPull+".png";
+        var dayTwoIconPull = (week.list[12].weather[0].icon);
+        var dayTwoIcon = "http://openweathermap.org/img/wn/"+dayTwoIconPull+".png";
+        var dayThreeIconPull = (week.list[20].weather[0].icon);
+        var dayThreeIcon = "http://openweathermap.org/img/wn/"+dayThreeIconPull+".png";
+        var dayFourIconPull = (week.list[28].weather[0].icon);
+        var dayFourIcon = "http://openweathermap.org/img/wn/"+dayFourIconPull+".png";
+        var dayFiveIconPull = (week.list[36].weather[0].icon);
+        var dayFiveIcon = "http://openweathermap.org/img/wn/"+dayFiveIconPull+".png";
+        // var k = week.main.temp
+        // var fahrenheit = (k -273.15) * 1.80 + 32;
+        // var newFahrenheit = fahrenheit.toFixed(1);
+        $(".fiveDay").html("");
+        $(".fiveDay").append(`
             <h3> 5-Day Forecast: </h3>
-                <div class ="weatherCard">
-                    <h4>${week.list[4].dt_txt}<h4>
-                    <h4>${week.city.name}<h4>
+                <div class ="dayOne">
+                    <p>${week.list[4].dt_txt}</p>
+                    <img src="${dayOneIcon}">
+                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
                     <p></p>
-                    <p></p>
+                </div>
 
+                <div class ="dayTwo">
+                    <p>${week.list[12].dt_txt}</p>
+                    <img src="${dayTwoIcon}">
+                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p></p>
+                </div>
+
+                <div class ="dayThree">
+                    <p>${week.list[20].dt_txt}</p>
+                    <img src="${dayThreeIcon}">
+                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p></p>
+                </div>
+
+                <div class ="dayFour">
+                    <p>${week.list[28].dt_txt}</p>
+                    <img src="${dayFourIcon}">
+                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p></p>
+                </div>
+
+                <div class ="dayFive">
+                    <p>${week.list[36].dt_txt}</p>
+                    <img src="${dayFiveIcon}">
+                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p></p>
+                </div>
 
         `);
     });
