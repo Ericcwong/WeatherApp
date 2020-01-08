bc3fc279311035e3ac4d2e36eeba0d24607980b6
Eric Wong <ericwong12@live.com>
2020-01-07 20:25:27 -0700

    Working on local storage saving the city is working, still needs work on saving the buttons value

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -15,13 +15,18 @@ $("#getWeather").on("click",function(event){
         var k = response.main.temp
         var fahrenheit = (k -273.15) * 1.80 + 32;
         var newFahrenheit = fahrenheit.toFixed(1);
+        var currentDayIconPull = (response.weather[0].icon);
+        var currentDayIcon = "http://openweathermap.org/img/wn/"+currentDayIconPull+".png";
         $(".currentDay").html("");
         $(".currentDay").append(`
-        <h3 id="cityName">${response.name}</h3>
-        <h5 id="temperature">Temperature: ${newFahrenheit} &#8457; </p>
-        <p id="humidity">Humidity: ${response.main.humidity}</p>
-        <p id="windSpeed">Wind Speed: ${response.wind.speed}</p>
-        <p id="uVIndex"></p>
+        <div class="currentDayCard">
+            <h3 id="cityName">${response.name}<img src="${currentDayIcon}"/></h3>
+            
+            <p id="temperature">Temperature: ${newFahrenheit} &#8457; </p>
+            <p id="humidity">Humidity: ${response.main.humidity}</p>
+            <p id="windSpeed">Wind Speed: ${response.wind.speed}</p>
+            <p id="uVIndex"></p>
+        </div>
         `);
         // $("#uVIndex").html() COME BACK TO THIS
     });
@@ -46,41 +51,60 @@ $("#getWeather").on("click",function(event){
         // var k = week.main.temp
         // var fahrenheit = (k -273.15) * 1.80 + 32;
         // var newFahrenheit = fahrenheit.toFixed(1);
+        var dayOneTempPull = (week.list[4].main.temp);
+        var fahrenheitOne = (dayOneTempPull -273.15) * 1.80 + 32;
+        var dayOneTemp = fahrenheitOne.toFixed(1);
+
+        var dayTwoTempPull = (week.list[12].main.temp);
+        var fahrenheitTwo = (dayTwoTempPull -273.15) * 1.80 + 32;
+        var dayTwoTemp = fahrenheitTwo.toFixed(1);
+
+        var dayThreeTempPull = (week.list[20].main.temp);
+        var fahrenheitThree = (dayThreeTempPull -273.15) * 1.80 + 32;
+        var dayThreeTemp = fahrenheitThree.toFixed(1);
+
+        var dayFourTempPull = (week.list[28].main.temp);
+        var fahrenheitFour = (dayFourTempPull -273.15) * 1.80 + 32;
+        var dayFourTemp = fahrenheitFour.toFixed(1);
+
+        var dayFiveTempPull = (week.list[36].main.temp);
+        var fahrenheitFive = (dayFiveTempPull -273.15) * 1.80 + 32;
+        var dayFiveTemp = fahrenheitFive.toFixed(1);
         $(".fiveDay").html("");
         $(".fiveDay").append(`
             <h3> 5-Day Forecast: </h3>
                 <div class ="dayOne">
                     <p>${week.list[4].dt_txt}</p>
                     <img src="${dayOneIcon}">
-                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p>Temp: ${dayOneTemp} 	&#8457; </p>
                     <p></p>
                 </div>
 
                 <div class ="dayTwo">
                     <p>${week.list[12].dt_txt}</p>
                     <img src="${dayTwoIcon}">
-                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p>Temp: ${dayTwoTemp} 	&#8457; </p>
                     <p></p>
                 </div>
 
                 <div class ="dayThree">
                     <p>${week.list[20].dt_txt}</p>
                     <img src="${dayThreeIcon}">
-                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p>Temp: ${dayThreeTemp} 	&#8457; </p>
                     <p></p>
                 </div>
 
                 <div class ="dayFour">
                     <p>${week.list[28].dt_txt}</p>
                     <img src="${dayFourIcon}">
-                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p>Temp: ${dayFourTemp} 	&#8457; </p>
                     <p></p>
                 </div>
 
                 <div class ="dayFive">
                     <p>${week.list[36].dt_txt}</p>
                     <img src="${dayFiveIcon}">
-                    <p>Temp: ${week.list[4].main.temp} 	&#8457; </p>
+                    <p>Temp: ${dayFiveTemp} 	&#8457; </p>
                     <p></p>
                 </div>
 
@@ -89,4 +113,18 @@ $("#getWeather").on("click",function(event){
 
 });
 
+$("#getWeather").on("click",function(){
+    var userInputsValue = $(this).siblings("#cityInput").val();
+    localStorage.setItem(userInputsValue,userInputsValue);
+    
+    $(".citySave").append(`<button class="reloadCity" value="${localStorage.getItem(userInputsValue)}">${localStorage.getItem(userInputsValue)}`)
 
+});
+
+// $(".saveBtn").on("click", function(event){
+//     var userInputsValue = $(this).siblings("textarea").val();
+//     var dataHour = $(this).siblings("textarea").attr("data-hour");
+//     localStorage.setItem(dataHour, userInputsValue);
+function saveCity(){
+    
+}
